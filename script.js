356b86e80a38f1368b2a369df588cd914dcdd7cd
jkoest8 <146107397+jkoest8@users.noreply.github.com>
2024-01-25 17:27:19 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function generateBarcode() {
+    var libraryNumber = document.getElementById('libraryNumber').value;
+    JsBarcode("#barcode", libraryNumber, {
+        format: "CODE128",
+        lineColor: "#000",
+        width: 2,
+        height: 40,
+        displayValue: false
+    });
+    document.getElementById('barcodeNumber').textContent = libraryNumber;
+}
