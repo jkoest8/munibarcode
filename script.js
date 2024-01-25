efd8253156ef2d0122f0b3b7b921c96e17ca0f19
jkoest8 <146107397+jkoest8@users.noreply.github.com>
2024-01-25 17:48:20 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ function generateBarcode() {
     JsBarcode("#barcode", libraryNumber, {
         format: "CODE128",
         lineColor: "#000",
-        width: 2,
-        height: 40,
+        width: 1, // Smaller width
+        height: 40, // Adjust height as needed
         displayValue: false
     });
     document.getElementById('barcodeNumber').textContent = libraryNumber;
